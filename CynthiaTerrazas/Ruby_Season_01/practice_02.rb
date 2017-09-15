c39b2bfb0f3d63074fca193cfc68d14808095a85
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-09-15 22:22:34 +0000

    my first commit

---
+++ CynthiaTerrazas/Ruby_Season_01/practice_02.rb
@@ -0,0 +1,11 @@
+class Practice_02
+  print "Give me a number: "
+  number = gets.chomp.to_f
+  puts "The floating number is: #{number}."
+
+  puts "if I send 5.0 float return 0.5."
+  puts "if I send 2one integer and string return 2.0. "
+  puts "if I send 27 integer  return 27.0"
+  puts "if I send one and string return 0.0."
+
+end
---
+++ CynthiaTerrazas/Ruby_Season_01/practice_1.rb
@@ -0,0 +1,14 @@
+class Practice_1
+
+
+  print "how old are you? "
+  age = gets.chomp.to_i
+  new_age = age + 9
+  puts "in nine years you will have #{new_age} years."
+
+  puts "if I send 27 string return 36"
+  puts "if I send 27years integer and string return 36 "
+  puts "if I send years27 integer and string return 9"
+  puts "if I send yo Iam27years integer and string return 9"
+
+end
